85367755d514701a2e901cfcb280ae3b92936f71
Deifante Walters <deifante@gmail.com>
2013-05-29 11:57:24 -0600

    More window dressing.
    Added a few tool tips for those not %100 in the know.
    Added a bunch more general information derived from the same back end info.

--- mongo_status/urls.py
+++ mongo_status/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, url
-from django.views.decorators.cache import cache_page
 
 from mongo_status import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
     url(r'^get_status/', views.get_status, name='get_status'),
-    url(r'^status_details/(?P<status>\w+)/$', cache_page(60*14)(views.complete_details), name='complete_details')
+    url(r'^status_details/(?P<status>\w+)/$', views.complete_details, name='complete_details')
     )
