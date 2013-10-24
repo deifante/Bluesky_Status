c7be30912cbff73c7f5afbbf14d8d6903fbb6744
Deifante Walters <deifante@gmail.com>
2013-10-24 15:50:02 -0600

    Added more api calls.

--- mongo_status_api/urls.py
+++ mongo_status_api/urls.py
@@ -1,9 +1,31 @@
 from django.conf.urls import patterns, url
 
-from mongo_status_api.views import BasicStatusListView, index
+from mongo_status_api import views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', index, name='index'),
-    url(r'^basic_status/$', BasicStatusListView.as_view(), name='basic_status'),
+    url(r'^$', views.index, name='index'),
+
+    # Basic Status URLs
+    url(r'^basic_status/$', views.BasicStatusListView.as_view()),
+    url(r'^basic_status/(?P<pk>\d+)/$', views.BasicStatusDetailView.as_view()),
+
+    # Status Count URLs
+    url(r'^status_count/$', views.StatusCountListView.as_view()),
+    url(r'^status_count/(?P<pk>\d+)/$', views.StatusCountDetailView.as_view()),
+
+    # Detailed Status URLs
+    url(r'^detailed_status/$', views.DetailedStatusListView.as_view()),
+    url(r'^detailed_status/(?P<pk>\d+)/$', views.DetailedStatusDetailView.as_view()),
+
+    # Day Summary URLs
+    url(r'^day_summary/$', views.DaySummaryListView.as_view()),
+    url(r'^day_summary/(?P<pk>\d+)/$', views.DaySummaryDetailView.as_view()),
+
+    # Sent Asset Summary URLs
+    url(r'^sent_asset_summary/$', views.SentAssetSummaryListView.as_view()),
+    url(r'^sent_asset_summary/(?P<pk>\d+)/$', views.SentAssetSummaryDetailView.as_view()),
+
+    # Exclusion List Urls
+    url(r'^exclusion_list/$', views.ExclusionListView.as_view()),
 )
--- mongo_status_api/views.py
+++ mongo_status_api/views.py
@@ -1,13 +1,41 @@
 import json
+import datetime
 
 from django.conf import settings
 from django.http import HttpResponse
 from django.core import serializers
+from django.db import models
 from django.views.generic.list import ListView, BaseListView
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+from django.views.generic.detail import BaseDetailView
 from django.db.models.query import QuerySet
+from django.forms.models import model_to_dict
+from bson.objectid import ObjectId
 
-from mongo_status.models import BasicStatus
+
+from mongo_status.mongo_access import MongoAccess
+from mongo_status.models import BasicStatus, StatusCount, DetailedStatus, DaySummary, SentAssetSummary
+from mongo_status.mysql_access import AbstractFile, User, AgencyContributorXUser
+
+class DateTimeJSONEncoder(json.JSONEncoder):
+    """
+    Allows for 8601 formatting of python datetime objects.
+
+    Causes the encoder to silently fail instead of throw
+    exceptions.
+    """
+    def default(self, o):
+        """
+        Allows for datetime object to be encoded as 8601.
+        """
+        try:
+            if isinstance(o, datetime.datetime):
+                return o.isoformat()
+            elif isinstance(o, ObjectId):
+                return str(o)
+            return json.JSONEncoder.default(self, o)
+        except TypeError:
+            return ''
 
 class JSONResponseMixin(object):
     """
@@ -19,14 +47,88 @@ class JSONResponseMixin(object):
         """
         response_kwargs['content_type'] = 'application/json'
         return HttpResponse(
-            self.convert_object_list_to_json(context),
+            self.convert_context_to_json(context),
             **response_kwargs
         )
-    def convert_object_list_to_json(self, context):
-        return serializers.serialize('json', context['object_list'])
+
+    def convert_context_to_json(self, context):
+        """
+        There's probably a better way to do this, but this works for now.
+
+        Right now we're replacing *every* QuerySet item in context with it's
+        equivalent dict. Also tossing most things that aren't handled by
+        python's JSON conversion.
+        """
+        for item in context:
+            if isinstance(context[item], QuerySet):
+                accumulator = []
+                for model_dict in context[item].values():
+                    accumulator.append(model_dict)
+                # now instead of a query set this is a list of dicts.
+                context[item] = accumulator
+
+            elif isinstance(context[item], models.Model):
+                # same concept here replace the model with a dict.
+                context[item] = model_to_dict(context[item])
+
+        datetime_encoder = DateTimeJSONEncoder(sort_keys=True, indent=2)
+        return datetime_encoder.encode(context)
+
+# Using the stuff above, define 2 class based views that can be reused
+# for displaying JSON data
+class JSONListView(JSONResponseMixin, BaseListView):
+    """
+    Provide functionality to display a list in JSON
+    """
+    pass
+
+class JSONDetailView(JSONResponseMixin, BaseDetailView):
+    """
+    Provide functionality to diplay a single object in JSON
+    """
+    pass
 
 def index(request):
     return HttpResponse(json.dumps("Working"), content_type='application/json')
 
-class BasicStatusListView(JSONResponseMixin, BaseListView):
+class BasicStatusListView(JSONListView):
+    queryset = BasicStatus.objects.filter(connection=settings.MONGO_HOST)[:5]
+
+class BasicStatusDetailView(JSONDetailView):
     queryset = BasicStatus.objects.filter(connection=settings.MONGO_HOST)
+
+class StatusCountListView(JSONListView):
+    queryset = StatusCount.objects.filter(connection=settings.MONGO_HOST)[:5]
+
+class StatusCountDetailView(JSONDetailView):
+    queryset = StatusCount.objects.filter(connection=settings.MONGO_HOST)
+
+class DetailedStatusListView(JSONListView):
+    queryset = DetailedStatus.objects.filter(connection=settings.MONGO_HOST)[:5]
+
+class DetailedStatusDetailView(JSONDetailView):
+    queryset = DetailedStatus.objects.filter(connection=settings.MONGO_HOST)
+
+class DaySummaryListView(JSONListView):
+    queryset = DaySummary.objects.filter(connection=settings.SPLUNK_HOST)[:5]
+
+class DaySummaryDetailView(JSONDetailView):
+    queryset = DaySummary.objects.filter(connection=settings.SPLUNK_HOST)
+
+class SentAssetSummaryListView(JSONListView):
+    queryset = SentAssetSummary.objects.filter(connection=settings.SPLUNK_HOST)[:5]
+
+class SentAssetSummaryDetailView(JSONDetailView):
+    queryset = SentAssetSummary.objects.filter(connection=settings.SPLUNK_HOST)
+
+class ExclusionListView(JSONResponseMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        mongo_access = MongoAccess()
+        mongo_exclusion_list = [{'userId':int(x['userId']), '_id':x['_id']} for x in mongo_access.get_exclusion_list()]
+        mysql_exclusion_list = [{'userId':x.user_id, 'agency_id':x.agency_id} for x in AgencyContributorXUser.objects.all()]
+        context = {
+            'exclusion_list':mongo_exclusion_list + mysql_exclusion_list,
+            'object_list':mongo_exclusion_list + mysql_exclusion_list
+        }
+        return self.render_to_response(context)
